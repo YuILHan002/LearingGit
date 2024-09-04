f3ae9900a1af210bbe70d3d175dc926a94846828
YuILHan002 <dharma.17teja@gmail.com>
2024-09-04 21:05:43 +0530

    First

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+
+        int a = st.pop();
+
+        System.out.println(a);
+
+
+    }
+    
+}
